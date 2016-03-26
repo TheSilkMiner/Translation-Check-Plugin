122d24afdf3a57d39cf180f163ffb5a4c514bb53
Bartek Bok <bartek.bok@gmail.com>
2016-03-26 20:00:17 +0100

    MC format codes validator

--- src/main/groovy/net/thesilkminer/gradle/plugin/translationchecker/Validators.groovy
+++ src/main/groovy/net/thesilkminer/gradle/plugin/translationchecker/Validators.groovy
@@ -46,6 +46,56 @@ class DuplicateTranslations implements Validator {
     def validateTranslation(String key, String value, ValidationMessageAppender addMessage) {}
 }
 
+@CompileStatic
+class McFormatCodes implements Validator {
+    static Set<Integer> toCodepoints(String... chs) {
+        return new HashSet<>(chs.collect {String ch -> ch.codePointAt(0) })
+    }
+
+    static final Set<Integer> allowedCodepoints = toCodepoints(
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', // colors
+        'k', // obfuscated
+        'l', // bold,
+        'm', // strikethrough
+        'n', // underline
+        'o', // italic
+        'r', // reset
+        '\u00A7', // escape
+    );
+
+    def validateLine(String value, ValidationMessageAppender addMessage) {
+        int i = 0
+        char[] chars = value.toCharArray()
+
+        while (i < chars.length) {
+            int codepoint = Character.codePointAt(chars, i)
+            i += Character.charCount(codepoint);
+            if (codepoint == 0xA7) {
+                if (i >= chars.length) {
+                    addMessage(i, "Format character at end of line")
+                    break
+                } else {
+                    codepoint = Character.codePointAt(chars, i)
+                    i += Character.charCount(codepoint);
+                    if (!(codepoint in allowedCodepoints))
+                        addMessage(i, "Illegal control code")
+                }
+            }
+        }
+    }
+
+    @Override
+    def validateTemplate(Set<String> keys, String value, ValidationMessageAppender addMessage) {
+        validateLine(value, addMessage)
+    }
+
+    @Override
+    def validateTranslation(String key, String value, ValidationMessageAppender addMessage) {
+        validateLine(value, addMessage)
+    }
+
+}
+
 interface ValidatorFactory {
     Validator create()
 }
@@ -60,6 +110,7 @@ class Validators {
         non_empty : wrap(NonEmptyValues.class),
         unused : wrap(UnusedTranslation.class),
         duplicate : wrap(DuplicateTranslations.class),
+        mc_format : wrap(McFormatCodes.class),
     ]
 
     static String[] getAllValidators() {
